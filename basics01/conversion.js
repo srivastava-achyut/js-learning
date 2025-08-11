f41f58054ec35074e474c0c6bd409c1c2cd347b4
Achyut Srivastava <achyutnishant08@gmail.com>
2025-08-11 13:42:40 +0000

    conversion string to integer integer to string

--- basics01/conversion.js
+++ basics01/conversion.js
@@ -1,24 +1,45 @@
-let age=33;
-console.log(typeof age);
-let score="13abc"; //string
-console.log(typeof score)
-let val=Number(score);
-console.log(typeof val);
-console.log(val); //NaN not a number
+// let age=33;
+// console.log(typeof age);
+// let score="13abc"; //string
+// console.log(typeof score)
+// let val=Number(score);
+// console.log(typeof val);
+// console.log(val); //NaN not a number
 
 //"33" => 33
 //"33abc" => NaN
 // true => 1; false => 0
-let isLoggedIn = 1;
+// let isLoggedIn = 1;
 
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);  // true
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);  // true
 // 1 => true
 // 0 =>false
 // "" => false 
 // "nishant" => true
 
-let someNumber=23;
-let stringnum=String(someNumber);
-console.log(someNumber); //23
-console.log(typeof stringnum);  // string
+// let someNumber=23;
+// let stringnum=String(someNumber);
+// console.log(someNumber); //23
+// console.log(typeof stringnum);  // string
+// let string="hello"
+// let str=" hello";
+// let str3=string+str;
+// console.log(str3);
+// let input1,input2,input3;
+// input1=10;
+// input2=10;
+// input3="10";
+// console.log(10+10+10);
+// console.log(10+"10"+10); //integer 10 convert into string "10" then "10"+"10"+"10"
+// console.log("10"+10+10); //101010 type is string
+// console.log("10"+10-10); //"10"+10=>"1010"-> "1010"-10=1000 type number
+// console.log("10"+10*10); //10100 first 10*10=100 convert into string "100"and then concat with "10"
+// console.log("10"-"10"); //0 number
+// console.log("10"-10*10);// 10-100 -90 number
+// console.log("10"+10/10);//101 string first 10/10=1 then 1 convert into string "1" and concat
+// console.log("10"+10%10);//10%10=0 -> "0" and then concat
+// let input=10+10+"10";//
+// console.log(typeof input);
+console.log(true);
+console.log(+true);
