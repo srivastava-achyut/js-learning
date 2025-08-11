9517b258857f02539369181c2a59f48c195248d9
Achyut Srivastava <achyutnishant08@gmail.com>
2025-08-11 04:01:13 +0000

    conversion datatypes

---
+++ basics01/conversion.js
@@ -0,0 +1,24 @@
+let age=33;
+console.log(typeof age);
+let score="13abc"; //string
+console.log(typeof score)
+let val=Number(score);
+console.log(typeof val);
+console.log(val); //NaN not a number
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1; false => 0
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);  // true
+// 1 => true
+// 0 =>false
+// "" => false 
+// "nishant" => true
+
+let someNumber=23;
+let stringnum=String(someNumber);
+console.log(someNumber); //23
+console.log(typeof stringnum);  // string
