669bcc3578f8addae369205a5bec5d011adc12b1
Achyut Srivastava <achyutnishant08@gmail.com>
2025-08-12 11:25:57 +0000

    typeof operator for datatype

---
+++ basics01/datatype.js
@@ -0,0 +1,27 @@
+// # Primitive datatype
+//7 category: String , Number, Boolean, NULL, undefined, Symbol, BigInt 
+
+// Reference (Non Primitive)
+// Array, Objects, Functions, 
+
+const score=100  // number number
+const scorevalue=100.3  //string string
+const isLoggedIn=false; // boolean boolean
+const outsideTemp=null; //null object
+let usermail; // undefined undefined
+const id=Symbol('123'); //symbol symbol
+const anotherID=Symbol('123');
+const bigNumber=34567890922445456890; // BigInt bigint
+const heros=["shaktiman", "nagraj","doga"]; //object
+let myobj={
+name:"Achyut", //object
+age:22,
+}
+const myFunction = function(){
+    console.log("Hello World");  //function
+}
+console.log(typeof bigNumber);
+console.log(typeof heros);
+console.log(typeof myobj);
+console.log(typeof id);
+console.log(typeof myFunction);
