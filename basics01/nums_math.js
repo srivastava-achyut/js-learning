5ab0ea44a0c36cd21cb9ab8f8d759d80a62e41c4
Achyut Srivastava <achyutnishant08@gmail.com>
2025-08-14 13:32:00 +0000

    maths and num

---
+++ basics01/nums_math.js
@@ -0,0 +1,27 @@
+const score=400;
+const balance=new Number(100);
+// console.log(balance);
+// console.log(score)
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2))
+const other=123.4567;
+//console.log(other.toPrecision(4));
+const hundreds=1000000;
+//console.log(hundreds.toLocaleString())
+//console.log(hundreds.toLocaleString('en-IN')) //indian format
+
+///+++++++++++++++++_______________MATH______MATH_________MATH______*********************
+
+// console.log(Math.abs(-4));  // 4 is ans convert positive into negative
+// console.log(Math.round(4.6)); // answer is 5 
+// console.log(Math.round(4.3)); //answer is 4 maths round of rule
+// console.log(Math.ceil(4.3)) // answer is 5 takes top value
+// console.log(Math.floor(4.2)) //answer is 4 takes lower value integer
+// console.log(Math.min(4,3,3,5,7,8)); // answer is 3 minimum of all element
+
+console.log(Math.random());
+console.log(Math.random()*10+1)
+
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random() * (max-min+1) )+min) //generates number between range 10 and 20
