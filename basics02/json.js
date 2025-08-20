a2f49c87dde1913999a50259eddd02f9bc4e0364
Achyut Srivastava <achyutnishant08@gmail.com>
2025-08-20 07:01:46 +0000

    object destructuring and basic of json

---
+++ basics02/json.js
@@ -0,0 +1,19 @@
+//object destructuring
+
+// const course={
+//     coursename:"JS",
+//     price:"1000",
+//     courseInstructor:"hitesh",
+// }
+const {courseInstructor}=course;
+console.log(courseInstructor); //rather using course.courseInstructor we are just using courseinstructor by the help of de structure
+const {courseInstructor : tutor}=course;
+console.log(tutor); //hitesh
+
+//API ************************* API**********************API*********************************
+{
+    name:"Achyut",
+    cousename;"Java Script",
+    price;"9000"
+
+}
