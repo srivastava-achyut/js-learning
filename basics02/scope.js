daf9b36714138faf4c2385d04bfcc474d4573256
Achyut Srivastava <achyutnishant08@gmail.com>
2025-08-21 08:18:27 +0000

    scope

---
+++ basics02/scope.js
@@ -0,0 +1,14 @@
+// let a=10
+// const b=20
+// var c=30
+// console.log(a);
+// console.log(b);
+// console.log(c);
+if(true){
+    let a=3;
+    const b=4;
+    var c=5;
+}
+console.log(a);//not defined because a is inside if and act as local scope 
+ console.log(b);
+console.log(c);
