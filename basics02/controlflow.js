03f8957c3d5d3e711beffc07ab9081e05ea8812d
Achyut Srivastava <achyutnishant08@gmail.com>
2025-08-23 14:48:09 +0000

    control flow switch if else ternary

---
+++ basics02/controlflow.js
@@ -0,0 +1,41 @@
+//if
+
+// const isuserloggedin =false;
+
+// if(isuserloggedin) {  //if isuserloggedin is true then console statement will run other console will not work
+//      console.log("hello")
+// }
+// // < > <= >= == === != !== && || 
+// const balance=500;
+// if(balance>600) console.log("test pass"); //console will not work
+
+// switch (key) {
+//      case value:
+          
+//           break;
+
+//      default:
+//           break;
+// }
+const month=3
+switch (month) {
+     case 1:
+          console.log("January");
+          break;
+     case 2:
+          console.log("february")
+          case 3:
+          console.log("March")
+          break;
+          case 4:
+          console.log("April")
+          break;
+
+     default:
+          console.log("case not match")
+          break;
+}
+const l1=5;
+const l2=6;
+console.log((l1>=l2)?l1:l2)
+
