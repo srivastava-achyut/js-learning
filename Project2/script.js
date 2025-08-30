0cf19b9c0191a0e85e95807410647a4313754270
Achyut Srivastava <achyutnishant08@gmail.com>
2025-08-30 12:49:40 +0000

    project 2

---
+++ Project2/script.js
@@ -0,0 +1,38 @@
+const form=document.querySelector('form')
+//parseInt(document.querySelector('#height').value);
+// this use will give empty value
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+  const height=  parseInt(document.querySelector('#height').value);
+  const weight=  parseInt(document.querySelector('#weight').value);
+  const results= document.querySelector('#results');
+
+  if(height === ''||height<0||isNaN(height)){
+       results.innerHTML="please give a valid height"
+  }
+  //results.innerHTML = '${height}';
+
+  else if(weight === ''||weight<0||isNaN(weight)){
+       results.innerHTML="please give a valid weight"
+  }
+  else{
+    //calculate BMI
+
+    const BMI=(weight/((height*height)/10000)).toFixed(2)
+  
+    //show the result
+    if(BMI<=18.6){
+         results.innerHTML= `<span>${BMI}</span><br>Under Weight`
+    }
+    
+    if(BMI<=24.9 && BMI>18.6){
+         results.innerHTML= `<span>${BMI}</span><br>Normal Range`
+    }
+if(BMI>24.9){
+         results.innerHTML= `<span>${BMI}</span><br>Over Weight`
+    }
+  }
+  
+})
