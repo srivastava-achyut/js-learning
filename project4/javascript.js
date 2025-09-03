30ca9667d76b297461e943b38442668d3a3bf952
Achyut Srivastava <achyutnishant08@gmail.com>
2025-09-03 19:13:14 +0000

    project 4 completed

--- project4/javascript.js
+++ project4/javascript.js
@@ -1,4 +1,4 @@
-const random=parseInt(Math.random()*100+1)
+let random=parseInt(Math.random()*100+1)
 const submit=document.querySelector('#subt')
 const userinput=document.querySelector('#guessField')
 
@@ -16,18 +16,75 @@ if(playgame){
         e.preventDefault();
         const guess=parseInt(userinput.value)
         console.log(guess)
-        validateGuess(guess)
+        validate(guess)
     })
 }
 function validate(guess){
-    
+    if(isNaN(guess)){
+        alert('Please enter a valid number')
+    }
+    else if(guess<1){
+ alert('Please enter a valid number')
+    }
+    else if(guess>100){
+        alert('Please enter a valid number less than 100')
+    }
+    else{
+        prevguess.push(guess)
+        if(attempt===11){
+            display(guess)
+            displaymess(`GAME OVER!! Random number was random`)
+            endgame()
+        }
+        else{
+            display(guess)
+            attempt++
+            checkguess(guess)
+        }
+    }
 }
 function checkguess(guess){
+      if(guess===random){
+        displaymess(`you guessed it right`)
+        endgame()
+      }
+    else  if(guess<random){
+        displaymess(`number is TOO low`)
+    }
+  else  if(guess>random){
+        displaymess(`number is TOO high`)
+    }
 
 }
 function display(guess){
-
+userinput.value=''
+  guesses.innerHTML+=`${guess} `
+  //attempt++;
+  remaininig.innerHTML=`${11-attempt}`
 }
 function displaymess(message){
+lowOrHi.innerHTML=`<h2>${message}</h2>`
+}
+function endgame(){
+  userinput.value=''
+  userinput.setAttribute('disabled','')
+  p.classList.add('button')
+  p.innerHTML=`<h2 id="newgame">Start new game</h2>`
+  startover.appendChild(p)
+  playgame=false
+  newgame()
+}
+function newgame(){
+const newgamebutton=document.querySelector('#newgame')
+newgamebutton.addEventListener('click',function(e){
+    random=parseInt(Math.random()*100+1)
+    prevguess=[]
+    attempt=1
+    guesses.innerHTML=''
+    remaininig.innerHTML=`${10-attempt}`
+    userinput.removeAttribute('disabled')
+    startover.removeChild(p)
+    playgame=true
 
+})
 }
