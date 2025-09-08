16fc5779461056d1797af70718e3a734be59380e
Achyut Srivastava <achyutnishant08@gmail.com>
2025-09-08 17:28:21 +0000

    project5

---
+++ project5/javascript.js
@@ -0,0 +1,24 @@
+const insert=document.getElementById('insert');
+window.addEventListener('keydown',(e)=>{
+insert.innerHTML=`
+      <div class="color>
+      <table>
+  <tr>
+    <th>Key</th>
+    <th>KeyCode</th>
+    <th>Code</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+</table>
+
+      </div>`
+})
